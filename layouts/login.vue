112a9a9f50bdf84547437eecf13e60869a05df76
Ilya Babunashvili <ilyabitru@gmail.com>
2022-07-06 20:41:20 +0300

    x

---
+++ layouts/login.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="h-full bg-white">
+    <slot />
+  </main>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+definePageMeta({
+  colorMode: 'light',
+  layout: 'login',
+})
+
+const { $loginUser } = useNuxtApp()
+</script>
+
+<template>
+  <div class="min-h-full flex">
+    <div class="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
+          <h2 class="mt-6 text-3xl font-extrabold text-gray-900">
+            Вход в личный кабинет
+          </h2>
+          <p class="mt-2 text-sm text-gray-600">
+            <!-- Or
+            {{ ' ' }}
+            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a> -->
+          </p>
+        </div>
+
+        <div class="mt-8">
+          <div>
+            <div>
+              <p class="text-sm font-medium text-gray-700">
+                Вход через
+              </p>
+
+              <div class="mt-1 grid grid-cols-1 gap-1">
+                <div>
+                  <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" @click.prevent="$loginUser({ provider: 'google' })">
+                    <span class="sr-only">Вход через Google</span>
+                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 1024 1024">
+                      <path fill-rule="evenodd" d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135c66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6c-63.8 0-117.8 43.1-137.1 101c-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101c33 0 61-8.7 82.9-23.4c26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1c0 74.7-26.7 137.4-73 180.1z" clip-rule="evenodd" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="mt-6 relative">
+              <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                <div class="w-full border-t border-gray-300" />
+              </div>
+              <div class="relative flex justify-center text-sm">
+                <span class="px-2 bg-white text-gray-500"> Или</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="mt-6">
+            <VVForm v-slot="{ meta: formMeta, errors: formErrors, values: formValues }" action="#" method="POST" class="space-y-6" @submit="$loginUser">
+              <div>
+                <label for="email" class="block text-sm font-medium text-gray-700"> Почта </label>
+                <div class="mt-1">
+                  <VVField id="email" name="email" rules="email" type="email" autocomplete="email" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                </div>
+              </div>
+
+              <div class="space-y-1">
+                <label for="password" class="block text-sm font-medium text-gray-700"> Пароль </label>
+                <div class="mt-1">
+                  <VVField id="password" name="password" rules="required" type="password" autocomplete="current-password" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <!-- <div class="flex items-center">
+                  <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                  <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Запомнить меня </label>
+                </div> -->
+
+                <div class="text-sm">
+                  <!-- <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a> -->
+                </div>
+              </div>
+
+              <div>
+                <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Войти
+                </button>
+              </div>
+            </VVForm>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="hidden lg:block relative w-0 flex-1">
+      <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="">
+    </div>
+  </div>
+</template>
